c6b24eac3d43608aea1ec864d6d5626d32a8953f
Malcolm Matalka <malcolm.matalka@acsl.se>
2026-09-25 10:52:02 +0200

    merged_apply_failed_dest_push_impacting_0006

--- tf/tf.tf
+++ tf/tf.tf
@@ -1,2 +1,12 @@
 resource "null_resource" "foo" {
 }
+
+resource "null_resource" "bar" {
+}
+
+# Fails when it is applied, after the plan succeeded, so that the apply reports a failed dirspace.
+resource "null_resource" "fail" {
+  provisioner "local-exec" {
+    command = "exit 1"
+  }
+}
